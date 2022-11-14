f83526ddbf44a314beb0484576adc4a983600549
NateSapoud <nathangdurocher@hotmail.com>
2022-11-14 11:46:09 -0500

    Update t_entree.c

--- TP2_INF155/t_entree.c
+++ TP2_INF155/t_entree.c
@@ -18,10 +18,15 @@ t_entree* t_entree_init(int num)
 	t_entree nouvelle_entree; //Creation d'une nouvelle entree
 	char nom[4] = "E"; //Creaton d'un char commencant toujours par E et qui ne depassera 4 caractere (E + les 2 chiffres possibles + \0)
 
-	nouvelle_entree.id = 1;
+	//set l'id au num fourni
+	nouvelle_entree.id = num;
 
-	strcat(nom, num);
+	//concatener le nom et l'id
+	strcat(nom, nouvelle_entree.id);
 	nouvelle_entree.nom = nom;
+
+	//initialiser une nouvelle pin de sortie
+	nouvelle_entree.pin = t_pin_sortie_init;
 
 	return &nouvelle_entree;
 
@@ -29,6 +34,44 @@ t_entree* t_entree_init(int num)
 
 void t_entree_destroy(t_entree* entree)
 {
+	//set tout les variables de la structure a NULL
 	entree->id = NULL;
 	entree->nom = NULL;
+	entree->pin = NULL;
+}
+
+t_pin_sortie* t_entree_get_pin(t_entree* entree)
+{
+	//retourne entree.pin puisque que le parametre est deja un pointeur
+	return entree->pin;
+}
+
+int t_entree_est_reliee(t_entree* entree)
+{
+	//verifie si la pin de sortie est relier
+	return t_pin_sortie_est_reliee(entree->pin);
+
+}
+
+void t_entree_reset(t_entree* entree)
+{
+	//reset la pin de sortie de l'entree en parametre
+	t_pin_sortie_reset(entree->pin);
+}
+
+int t_entree_propager_signal(t_entree* entree)
+{
+	//si la valeur de la pin de sortie est a -1
+	if (t_pin_sortie_get_valeur(entree->pin) == -1)
+	{
+		//retourne 1 si le signal a reussi a se propager, 0 sinon
+		return t_pin_sortie_propager_signal(entree->pin);
+	}
+}
+
+int t_entree_get_valeur(t_entree* entree)
+{
+	//retourne la valeur de la pin de sortie associer avec la l'entree en parametre
+	return t_pin_sortie_get_valeur(entree->pin);
+
 }
