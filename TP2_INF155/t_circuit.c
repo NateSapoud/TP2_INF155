1ee84db26147cad9f1399ceb9cb19b6fc697f77b
NateSapoud <nathangdurocher@hotmail.com>
2022-11-28 10:07:13 -0500

    Create t_circuit.c

---
+++ TP2_INF155/t_circuit.c
@@ -0,0 +1,13 @@
+/*
+Titre: t_circuit.c
+Description: Programme qui implémente les fonctions de la structure t_entree.
+
+Auteur: Nathan Guindon Durocher - William Blais Di Quinzio - Tri Tam Phan
+Date: 2022-11-14
+*/
+
+/***********COMMANDES PRÉPROCESSEUR*************/
+#include "t_circuit.h"
+
+
+/********* DÉFINITION DES FONCTION **************/
