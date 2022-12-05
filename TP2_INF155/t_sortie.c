b2f4d8a1e4f976ae85b10816900be5ee2b19d5f9
NateSapoud <nathangdurocher@hotmail.com>
2022-12-05 11:19:17 -0500

    Update t_sortie.c

--- TP2_INF155/t_sortie.c
+++ TP2_INF155/t_sortie.c
@@ -15,7 +15,7 @@ Date: 2022-11-28
 t_sortie* t_sortie_init(int num)
 {
 	t_sortie* nouvelle_sortie; //Cree une nouvelle sortie
-	char nom[3] = "S"; //Nom de la sortie comportant 3 charactere; le premier qui est "S" (pour sortie), le deuxieme qui va etre le chiffre, et le troisieme qui est le \0
+	char nom[4] = "S"; //Nom de la sortie comportant 4 charactere; le premier qui est "S" (pour sortie), le deuxiemee et troisiem qui vont etre les chiffres, et le quatrieme qui est le \0
 	nouvelle_sortie = (t_sortie*)malloc(sizeof(t_sortie));
 
 	if (nouvelle_sortie == NULL) //va verifier si la memoire est epuisee ou non. si elle est epuisee, quitte la fonction
@@ -27,14 +27,13 @@ t_sortie* t_sortie_init(int num)
 
 	nouvelle_sortie->id = num; //la valeur "id" du struct "t_sortie" va prendre la valeur du numero que l'utilisateur a mis 
 
-	nouvelle_sortie->nom = "S%d", num; //Le strcat concatène la chaine de charactere "nom" et la chaine "num"
-	 //la valeur "nom" du stuct "t_sortie" va prendre les characteres du strcat. (va prendre, par exemple, S2\0, qui represente la sortie 2
+	nouvelle_sortie->nom = "S%d", num; //Assignation du nom de la nouvelle sortie
+	 
 	
 
-	nouvelle_sortie->pin = t_pin_sortie_init; //Crée une nouvelle pin sortie.
+	nouvelle_sortie->pin = t_pin_entree_init; //Crée une nouvelle pin sortie.
 
 	return nouvelle_sortie; //retourne le pointeur vers la sortie de circuit créée
-
 }
 
 void t_sortie_destroy(t_sortie* sortie)
@@ -50,7 +49,6 @@ t_pin_entree* t_sortie_get_pin(t_sortie* sortie)
 
 int t_sortie_relier(t_sortie* dest, const t_pin_sortie* source) 
 {
-
 	t_pin_entree_relier(dest->pin,source);
 
 	if (t_pin_entree_est_reliee(dest->pin))
